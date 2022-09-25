b0d56fd406bbb330ba9888201358deabe3f0f1ba
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-09-24 23:38:44 -0300

    Feature: Import ing links from devGo Created with fetch and cheerio

---
+++ server/src/modules/links/services/importLink/ImportLinkController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+import { container } from "tsyringe";
+
+import { ImportLinkService  } from "./ImportLinkService";
+
+class ImportLinkController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const importLinkService = container.resolve(ImportLinkService);
+
+        const { url } = request.body
+
+        const result = await importLinkService.execute(url);
+
+        if(result === false ){
+            return response.status(400).json({
+                "erro": "Não foi possível importar os links"
+            });  
+        }
+
+        return response.status(201).send();
+
+    }
+}
+
+export { ImportLinkController };
