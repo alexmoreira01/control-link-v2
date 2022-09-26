b01f2cd45bbdb9687f88ee0f6336f749918ae2a5
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-09-25 23:15:09 -0300

    Feature: Change url to domain

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
     // baseURL: 'http://localhost:3333/link',
-    baseURL: 'http://54.211.140.125/link',
+    // baseURL: 'http://54.211.140.125/link',
+    baseURL: 'https://dev.freelancerdesucesso.fun/link',
 });
