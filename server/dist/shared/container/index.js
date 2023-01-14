16d7020aabef41450711cffa0f6b646850d0a31b
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-13 23:40:10 -0300

    Remove folder dist

--- server/dist/shared/container/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _LinksRepository = require("../../modules/links/repositories/LinksRepository");
-
-_tsyringe.container.registerSingleton("LinksRepository", _LinksRepository.LinksRepository);
