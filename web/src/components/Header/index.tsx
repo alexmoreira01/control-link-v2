f2ec8f8f905221883136f5c4816e8379f0d93b67
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-16 23:21:22 -0300

    Refactory: Components header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,50 +1,47 @@
 
-import iconLink from '../../assets/link.svg'
-import { api } from '../../services/api';
+// interface ImportLinkModalProps {
+//     onReloadLinksRequest?: () => void;
+//     handleActiveLoading?: () => void;
+//     handleDesactiveLoading?: () => void;
+//   }
 
-interface ImportLinkModalProps {
-    onReloadLinksRequest: () => void;
-    handleActiveLoading: () => void;
-    handleDesactiveLoading: () => void;
-  }
+//     // async function handleImportLinkDevGo(){
+//     //   handleActiveLoading();
 
-export function Header({onReloadLinksRequest, handleActiveLoading, handleDesactiveLoading}: ImportLinkModalProps) {
+//     //     try {
+//     //       await api.post('/devGo/import', {
+//     //         url: "https://devgo.com.br/"
+//     //       })
 
-    async function handleImportLinkDevGo(){
-      handleActiveLoading();
+//     //       onReloadLinksRequest();
+//     //     } catch (err) {
+//     //       alert("Não foi possível importar os links!")
+//     //     }
 
-        try {
-          await api.post('/devGo/import', {
-            url: "https://devgo.com.br/"
-          })
-    
-          onReloadLinksRequest();
-        } catch (err) {
-          alert("Não foi possível importar os links!")
-        }
+//     //     handleDesactiveLoading()
+//     //   }
 
-        handleDesactiveLoading()
-      }
+import { HeaderContainer } from "./styles";
 
+import linkLogo from "../../assets/link.svg";
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
     return (
-        <div className="mb-16 mx-5 my-5">
-            <div className=" m-auto relative flex items-center justify-between whitespace-nowrap">
-                <div className='flex items-center '>
-                    <img src={iconLink} alt="link icon" />
-                    <h1 className="text-3xl text-white font-bold">ControlLinks</h1>
-                </div>
-    
-                <button 
-                    className='className="text-2sm text-white font-semibold bg-green-500 rounded-md px-4 py-3 hover:bg-green-600" type="button"'
-                    onClick={handleImportLinkDevGo}
-                >
-                    Importar links DevGo
-                    
-                </button>
-                
-
-                
+        <HeaderContainer>
+            <div>
+                <img src={linkLogo} alt="" />
+                <h1>Control-Links</h1>
             </div>
-        </div>
-    )
+
+            <button
+
+            // onClick={handleImportLinkDevGo}
+            >
+                Importar links DevGo
+
+            </button>
+        </HeaderContainer>
+    );
 }
