84c3c1902517d79a86dc08e1c8e6072ec46ca0c4
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-17 22:48:34 -0300

    Styles: Update

--- web/src/layout/DefaultLayout/styles.ts
+++ web/src/layout/DefaultLayout/styles.ts
@@ -3,29 +3,22 @@ import styled from "styled-components";
 export const LayoutContainer = styled.div`
   display: flex;
   justify-content: center;
-  /* align-content: center; */
-  /* flex-wrap: wrap; */
-  
- 
   height: 150px;
   margin: 0;
   background-color: ${(props) => props.theme["gray-900"]}; 
-
-
- 
 `;
 
 export const ContainerContent = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 74rem;
   max-width: 74rem;
-  /* height: calc(100vh - 8rem); */
   height: fit-content;
-  margin: 5rem auto;
-  padding: 2.5rem;
+  
+  margin: 3rem auto;
+  padding: 2.5rem 2.5rem 0.4rem 2.5rem;
 
   background: ${(props) => props.theme["gray-750"]};
   border-radius: 8px;
 
-  display: flex;
-  flex-direction: column;
 `;
