dbb66563558b3fb309945b489aa0c505338a4a2c
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-22 22:58:40 -0300

    update api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333/link',
+    // baseURL: 'http://localhost:3333/link',
     // baseURL: 'http://54.211.140.125/link',
-    // baseURL: 'https://dev.freelancerdesucesso.fun/link',
+    baseURL: 'https://api-control-links-v1.herokuapp.com/link',
 });
