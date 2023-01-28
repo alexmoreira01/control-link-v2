d0a95aea8bae0e5b614f465f21f16f2784b14c9f
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-28 20:36:16 -0300

    Feature: middleware error

--- server/src/infra/app.ts
+++ server/src/infra/app.ts
@@ -1,20 +1,37 @@
 import "reflect-metadata";
 import "dotenv/config";
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors"; 
+
+import { AppError } from "./errors/app-error";
+
 import cors from 'cors';
 
 import "./container";
-import { AppDataSource } from "./database/typeorm/index";
 
 import { router } from "./http/routes";
 
-AppDataSource; // Conexão
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
 app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
+        });
+    }
+);
 
 export { app };
