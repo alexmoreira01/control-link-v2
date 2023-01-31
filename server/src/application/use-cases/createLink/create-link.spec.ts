52e62daede6d90a43898a2129f82785eab2ae82f
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-01-30 23:36:35 -0300

    Test: create link test

---
+++ server/src/application/use-cases/createLink/create-link.spec.ts
@@ -0,0 +1,37 @@
+import { LinksRepositoryInMemory } from "../../../../test/repositories/in-memory-links-repository";
+import { AppError } from "../../../infra/errors/app-error";
+import { CreateLink } from "./create-link";
+
+let createLink: CreateLink;
+let linksRepositoryInMemory: LinksRepositoryInMemory;
+
+describe("Create link", () => {
+    beforeEach(() => {
+        linksRepositoryInMemory = new LinksRepositoryInMemory();
+        createLink = new CreateLink(linksRepositoryInMemory)
+    }); 
+
+    it('should be able to create a new link', async () => {
+        const { link } = await createLink.execute({
+            label: "label a link",
+            url: "url a link"
+        });
+
+        // expect(linksRepositoryInMemory.links).toHaveLength(1);
+        expect(link).toHaveProperty("id");
+    });
+
+    it("should not be able to create a link with title equals", async () => {
+        await createLink.execute({
+            label: "labelTest",
+            url: "urlTest2"
+        }); 
+
+        await expect(
+            createLink.execute({
+                label: "labelTest",
+                url: "urlTest2"
+            })
+        ).rejects.toEqual(new AppError("Title already exists!"));
+    })
+})
