e821070a9087e5591b7f08596f622a33a5acbeaa
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-02-02 22:46:49 -0300

    Docs: Init documentation with swagger

--- server/src/infra/app.ts
+++ server/src/infra/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors"; 
+import swaggerUi from "swagger-ui-express"
 
 import { AppError } from "./errors/app-error";
 
@@ -11,10 +12,13 @@ import cors from 'cors';
 import "./container";
 
 import { router } from "./http/routes";
+import swaggerFile from "../swagger.json"
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(cors());
 app.use(router);
